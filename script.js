2dd698b11c531c51a5ba1b1456f5ea8affcf46d3
jajamarianelli-hub <jajamarianelli@gmail.com>
2026-06-27 21:23:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-a
+const bottone = document.getElementById("compraBtn");
+
+bottone.addEventListener("click", function () {
+    alert("Grazie per il tuo interesse!");
+
+    // Reindirizza alla pagina desiderata
+    window.location.href = "https://google.com";
+});
